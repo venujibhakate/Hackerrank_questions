34b11282a2473a49b69c991a0577c72d40e7af20
venujibhakate <45447075+venujibhakate@users.noreply.github.com>
2019-11-07 15:28:32 +0530

    Update socks.py

--- socks.py
+++ socks.py
@@ -12,17 +12,3 @@ def socksMarchant(list1):
         i = i + 1
     return(count)
 print(socksMarchant([1,2,3,3,1,1,1,3,4,6,6]))
-
-#  socks = {}
-#     res = 0
-    
-#     for el in ar:
-#         if el not in socks.keys():
-#             socks[el] = 1
-#         else:
-#             socks[el] += 1
-    
-#     for key in socks.keys():
-#         res += socks[key]//2
-    
-#     return res
